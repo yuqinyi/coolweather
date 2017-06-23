f555e364647f416c9b12e9331deddf453337a419
yuqinyi <yuqinyi@hotmail.com>
2017-06-23 13:13:59 +0800

    修复状态栏透明变成灰块。

--- app/src/main/java/com/rdx64/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/rdx64/coolweather/android/WeatherActivity.java
@@ -53,9 +53,9 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= 21) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_weather);
